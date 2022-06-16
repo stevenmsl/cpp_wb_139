d629b1521835b90b5ff25b473d23dbca11c56566
Steven Lin <steven_lx@outlook.com>
2022-06-16 10:11:48 -0700

    add more comments

--- solution.cpp
+++ solution.cpp
@@ -14,6 +14,16 @@ using namespace std;
 /*takeaways
   - DP
     - recursion and memorize what has been evaluated
+
+  - break the string in all possible locations
+    - mem = ["leet","code"], s = "leetcode"
+    - "" + "leetcode" -> "leetcode" is not in the mem (X)
+    - "l" + "eetcode" -> "eetcode" is not in the mem (X)
+    - "le" + "etcode" -> "etcode" is not in the mem (X)
+    - ...
+    - "leet" + "code" -> "code" is in the mem, check "leet"
+      recursively
+
   - time complexity : O(n^2)
 */
 
@@ -21,7 +31,10 @@ bool Solution::wordBreak(string s, vector<string> &wordDict)
 {
   /* remember what has been calculated */
   auto mem = unordered_map<string, bool>();
-  /* base */
+  /* base cases
+     - all the words in the dictionary are
+       considered evaluated
+  */
   for (auto w : wordDict)
     mem[w] = true;
 
@@ -40,6 +53,12 @@ bool Solution::_wb(const string &s, unordered_map<string, bool> mem)
      from all possible locations
      - we already dealt with the case where
        the left is an empty string in the above
+       - s = "" + s;
+     - the idea is to check a longer string (right)
+       quickly to see if it's in the mem
+     - only if the longer string is legit we
+       will check the left further
+
   */
   for (auto i = 1; i < s.size(); i++)
   {
